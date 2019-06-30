c7b1efc79f7c6668a013d8baad3842c0e2ece23f
Rachel BENYOUCEF <rbe@dci.fr>
2019-06-30 23:20:13 +0200

    Tableau employee & category

--- assets/vue/components/Employee.vue
+++ assets/vue/components/Employee.vue
@@ -1,98 +1,85 @@
 <template>
     <div class="card w-100 mt-2" >
-        <div class="card-body" >
-            <form >
-            Nom : <strong>{{ lastname }}</strong> Prénom : <strong>{{ firstname }} </strong>Site :  <strong>{{site }}</strong>
-            
-            <button type="button" class="btn btn-danger" data-toggle="modal" style="right: 100px;position: absolute;width:120px;" @click="deleteModal(id,lastname)" 
-              >
-                <i class="fa fa-trash"></i> Supprimer
-            </button>
-            <button type="button" class="btn btn-primary" data-toggle="modal" style="right: 230px;position: absolute;width:110px;" @click="openModal(id)" 
-              >
-                <i class="fa fa-edit"></i> Modifier
-            </button>
-            <input type="hidden" id="id" name="id" class="form-control" :value="id">
-            <!-- <button type="button" class="btn btn-warning" v-if="!isActive" style="right: 20px;position: absolute;width:70px;" @click="activateEmployee(id)">Inactif
-            </button>
-            <button type="button" class="btn btn-success" v-else style="right: 20px;position: absolute;width:70px;" @click="unactivateEmployee(id)">Actif</button> -->
-            
-            </form>
-        </div>
+        <tr>
+            <td>{{lastname }}</td>
+            <td>{{firstname }}</td>
+            <td>{{site }}</td>
+            <td>
+                <button type="button" class="btn btn-danger" data-toggle="modal"  @click="deleteModal(id,lastname)" >
+                    <i class="fa fa-trash"></i> Supprimer
+                </button>
+            </td>
+            <td>
+                <button type="button" class="btn btn-primary" data-toggle="modal"  @click="openModal(id)">
+                    <i class="fa fa-edit"></i> Modifier
+                </button>
+            </td>        
+        </tr>
         <div class="modal fade bg-dark" tabindex="-1" role="dialog" aria-hidden="true" :id="'bv-modal-example'+id">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" >Modifier</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                </div>
-                <div class="modal-body">
-                    <div class="col-12">
-                                <div class="col-6">
-                                    <input v-model="lastname" type="text" class="form-control">
-                                </div>
-                                <div class="col-6">
-                                    <input v-model="firstname" type="text" class="form-control">
-                                </div>
-                                <div class="col-6">
-                                    <input v-model="site" type="text" class="form-control">
-                                </div>
-                               <!--  <div class="col-6">
-                                    <select class="form-control" name="category" v-model="category" >
-                                      <option v-for="Otheruser in users" v-bind:value="Otheruser.id" >
-                                      {{ Otheruser.id }}
-                                      </option>
-                                      
-                                    </select>
-                                </div> -->
-                                <!-- <input type="hidden" id="isActive" name="isActive" class="form-control" :value="isActive"> -->
-
+                    <div class="modal-header">
+                        <h4 class="modal-title" >Modifier</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="col-12">
+                            <div class="col-6">
+                                <input v-model="lastname" type="text" class="form-control">
                             </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light waves-effect" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-success waves-effect waves-light" data-dismiss="modal"
-                            @click="editEmployee(id,firstname,lastname,site)">
-                    Modify
+                            <div class="col-6">
+                                <input v-model="firstname" type="text" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <input v-model="site" type="text" class="form-control">
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light waves-effect" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-success waves-effect waves-light" data-dismiss="modal"
+                        @click="editEmployee(id,firstname,lastname,site)">
+                        Modify
                     </button>
                 </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
-
-          <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true"
-         style="display: none;"
-         :id="'delete-employee'+id">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title" >Delete Employee</h4>
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          </div>
-          <div class="modal-body">
-            Are you sure that you want to delete this employee ?
-            <ul>
-              <li >
-                {{ firstname }} {{ lastname }}
-              </li>
-            </ul>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-light waves-effect" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-danger waves-effect waves-light" data-dismiss="modal"
-                    @click.prevent="deleteEmployee(id)">
-              Delete
-            </button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-    </div>
-    
+
+    <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true"
+    style="display: none;"
+    :id="'delete-employee'+id">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" >Delete Employee</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            </div>
+            <div class="modal-body">
+                Are you sure that you want to delete this employee ?
+                <ul>
+                    <li >
+                        {{ firstname }} {{ lastname }}
+                    </li>
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light waves-effect" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger waves-effect waves-light" data-dismiss="modal"
+                @click.prevent="deleteEmployee(id)">
+                Delete
+            </button>
+        </div>
+    </div><!-- /.modal-content -->
+</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+</div>
+
 </template>
 
 <script>
-    
+
     export default {
         name: 'employee',
         props: ['id','lastname','firstname','site'],
