4cf29d3f15b3bcbf67a6ce886396e1ab92ad4141
Douae El Omari <douae.elomari@orange.com>
2019-07-08 11:54:30 +0200

    Adding pagination

---
+++ assets/vue/components/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="overflow-auto">
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+    <b-table striped hover
+      id="my-table"
+      :items="dashboards"
+      :per-page="perPage"
+      :current-page="currentPage"
+      small
+    ></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        perPage: 10,
+        currentPage: 1,
+        fields: ['Actions', 'Date'],
+      }
+    },
+    created () {
+    this.$store.dispatch('dashboard/fetchDashboards');
+
+  },
+    computed: {
+      rows() {
+        return this.dashboards.length
+      },
+      dashboards () {
+      return this.$store.getters['dashboard/dashboards'];
+    },
+    }
+  }
+</script>
