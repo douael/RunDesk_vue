2db714b4780f46f0c57ebc3975e546d4eb5e7d69
Douae El Omari <douae.elomari@orange.com>
2019-07-16 10:30:07 +0200

    Loading CSS to all loaders

--- assets/vue/views/Type.vue
+++ assets/vue/views/Type.vue
@@ -22,7 +22,13 @@
         </div>
 
         <div v-if="isLoading" class="row col">
-            <p>Chargement...</p>
+            <div class="e-loadholder">
+                <div class="m-loader">
+                    <span class="e-text">Loading</span>
+                </div>
+            </div>
+            <div id="particleCanvas-Blue"></div>
+            <div id="particleCanvas-White"></div>
         </div>
 
         <div v-else-if="hasError" class="row col">
