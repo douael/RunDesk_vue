7808f6bf3b5e8f46a90124b40af3273325cf273d
Douae El Omari <douae.elomari@orange.com>
2019-07-29 17:26:33 +0200

    Fix

--- assets/vue/components/Dashboard.vue
+++ assets/vue/components/Dashboard.vue
@@ -11,7 +11,7 @@
 
     <b-table striped hover
       id="my-table"
-      :items="dashboards.slice(0, 20)"
+      :items="dashboards.slice(0, 20).reverse()"
       :per-page="perPage"
       :current-page="currentPage"
       small
